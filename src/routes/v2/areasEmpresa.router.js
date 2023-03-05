99ed80457259064e3c82da81fed03db5c69d3748
carlosSploit <arturo14212000@gmail.com>
2023-03-05 16:40:57 -0500

    05/03/23

---
+++ src/routes/v2/areasEmpresa.router.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const rooutes = express.Router()
+const Negempresa = require('../../negocio/v2/empresa.negocio')
+const objempresa = new Negempresa()
+// ######################### rooutes ###################################
+
+// List
+rooutes.get('/:id_clienAnalit', async (req, res) => {
+  objempresa.list_empresa(req, res)
+})
+
+// read
+// rooutes.get('/read/:id_clienAnalit', async (req, res) => {
+//   objempresa.read_clienAnalit(req, res)
+// })
+
+// insertar
+rooutes.post('/', async (req, res) => {
+  objempresa.inser_empresa(req, res)
+})
+
+// insertar enlace
+rooutes.post('/enlace', async (req, res) => {
+  objempresa.inser_empresa_enlace(req, res)
+})
+
+// delete
+rooutes.delete('/:id_clienAnalit/:id_empresa', async (req, res) => {
+  objempresa.eliminar_empresa_enlace(req, res)
+})
+// actualizar
+// rooutes.put('/:id_clienAnalit', async (req, res) => {
+//   objempresa.actuali_clienAnalit(req, res)
+// })
+
+// actualizar
+rooutes.put('/:id_empresa', async (req, res) => {
+  objempresa.actuali_empresa(req, res)
+})
+
+module.exports = rooutes
