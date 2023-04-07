53da6683c6acb1faa778cceceeeabae46d9797af
carlosSploit <arturo14212000@gmail.com>
2023-04-07 17:06:01 -0500

    07/04/23

--- config/config.js
+++ config/config.js
@@ -18,16 +18,16 @@ const config = {
       pool: {
         multipleStatements: true,
         connectionLimit: 5,
-        // "host"    : keysconfig.bdcpanel.HOST,
-        // "port"    : keysconfig.bdcpanel.PORT,
-        // "user"    : keysconfig.bdcpanel.USSER,
-        // "password": keysconfig.bdcpanel.PASSWORD,
-        // "database": keysconfig.bdcpanel.DATABASE
-        host: 'localhost',
-        port: 3306,
-        user: 'root',
-        password: '',
-        database: 'sgpc'
+        host: keysconfig.raiwaiy.HOST,
+        port: keysconfig.raiwaiy.PORT,
+        user: keysconfig.raiwaiy.USSER,
+        password: keysconfig.raiwaiy.PASSWORD,
+        database: keysconfig.raiwaiy.DATABASE
+        // host: 'localhost',
+        // port: 3306,
+        // user: 'root',
+        // password: '',
+        // database: 'sgpc'
       }
     }
   },
