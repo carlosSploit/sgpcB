75df93708fb99efec1158c3c577f254d01d9b0db
carlosSploit <arturo14212000@gmail.com>
2023-04-12 12:32:45 -0500

    12/04/23

--- config/tockenizer/tokenizer.js
+++ config/tockenizer/tokenizer.js
@@ -14,7 +14,7 @@ module.exports = function verifyToken (req, res, next) {
     const token = listdatetoken[listdatetoken.length - 1]
     // console.log(req.headers.authorization)
     const payload = jwt.decode(token, config.apidatkey)
-    // console.log('payload', payload)
+    console.log('payload', payload)
     if (payload == null) return res.status(403).send({ message: 'No tienes autorizacion' })
     // coomprobamos la existencia de las key y sus valores dados, entre el json esperado y el json desifrado
     for (const key in config.apidatkey) {
